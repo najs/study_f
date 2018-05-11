ffa4f80652448c81d750d52e8647c2d65c2de80e
najs <na.jss@daum.net>
2018-05-12 01:49:05 +0900

    add day_5th homework 함수 분리 수정?

--- day_5th/hw/object.js
+++ day_5th/hw/object.js
@@ -37,12 +37,12 @@ var imagePath = response.i;
 var members = response.l;
 
 //랜덤숫자 뽑기
-var makeRandom = function(max, min){
+var makeRandomNumber = function(max, min){
 	var randomNumber = Math.random()*(max - min) + min;
 	return parseInt(randomNumber,10);
 };
 
-//새로운 데이터 뽑기
+//데이터 뽑기
 var selectData = function(n){
 	var samplingMembers = [];
 	for(var i = 0; i < n ; i++){
@@ -51,31 +51,23 @@ var selectData = function(n){
 	return samplingMembers;
 };
 
-//뿌리기
-var drawMembers = function(){
-	var membersCount = makeRandom(0,totalCount);
-	var newMemberData = selectData(membersCount);
-	var memberLiElement = newMemberData.map(function(v){
+//새로운 데이터 만들기
+function makeData(num){
+	return selectData(num);
+}
+
+//그리기
+var drawMembers = function(memberCount, data){
+	var memberLiElement = data.map(function(v){
 		var src = v.p !== 'N' ? imagePath + v.p : '';
 		var className = src ? '': 'no-image';
 		return '<li class="'+ className +'"><span><img src="'+ src +'"></span><span>' + v.n + '</span></li>';
 	}).join('');
-
-	document.body.innerHTML = '<div>접속 맴버 : ' + membersCount + '명 <ul>' + memberLiElement + '</ul></div>';
+	document.body.innerHTML = '<div>접속 맴버 : ' + memberCount + '명 <ul>' + memberLiElement + '</ul></div>';
 };
 
+
 setInterval(function(){
-	drawMembers();
-},1000);
-
-
-/*
- 1	리팩토링을 먼저 한다. (리팩토링은 동작하는 결과물을 망가뜨리지 않는다.
- 흩뿌려진 코드를 함수로 묶거나 함수의 이름을 변경하거나 분기문의 위치를 이동하거나 한다.
- 이미 만들어진 함수의 인풋과 아웃풋을 수정해야 한다면 다시 생각할 필요가 있다)
-
- 2	데이터를 준비하는 부분과 데이터를 만드는(필터링) 부분과 그리는 부분을 구분한다.
- 3	setInterval 내에 데이터를 만드는 부분과 그리는 부분이 있으면 그대로 그려진다.
- 4	parseInt('40', 10);    // '40' 이라는 문자열을 10진수 숫자 40으로 변환하는 코드 예제
- 5	어떻게 하면 사람들을 랜덤하게 구할 수 있을 것인가 고민한다
- */
+	var randomCount = makeRandomNumber(0,totalCount);
+	drawMembers(randomCount, makeData(randomCount));
+}, 1000);
